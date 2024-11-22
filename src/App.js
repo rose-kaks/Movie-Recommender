15f99964e1527b82025d7a7ef3338c9d88638a96
rose-kaks <rosekakrania@gmail.com>
2024-11-22 12:32:30 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Watchlist from "./pages/Watchlist";
 
-function App() {
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/watchlist" element={<Watchlist />} />
-            </Routes>
-        </Router>
-    );
-}
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  const handleLogout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+  };
+
+  return (
+    <Router>
+      <Navbar user={user} onLogout={handleLogout} />
+      <Routes>
+        <Route path="/" element={<Home user={user} />} />
+        <Route path="/watchlist" element={<Watchlist user={user} />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
