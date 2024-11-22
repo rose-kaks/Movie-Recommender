824ad58eeae95fdc56324ad65fd99173eec304e3
radhika2421 <144546481+radhika2421@users.noreply.github.com>
2024-11-22 14:06:40 +0530

    delete api

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { signupUser } from "../utils/api";
 
 const SignupPage = () => {
     const [name, setName] = useState("");
