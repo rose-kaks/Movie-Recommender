4f7a0761b63de784d6ea9edfe14ca03a028cdafa
radhika2421 <144546481+radhika2421@users.noreply.github.com>
2024-11-22 14:06:17 +0530

    delete api

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { loginUser } from "../utils/api";
 
 const LoginPage = () => {
     const [email, setEmail] = useState("");
